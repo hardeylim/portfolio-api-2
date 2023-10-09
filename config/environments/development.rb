8626df427de9e178e4f60fd3bc6d6cfe752c42c8
hardeylim <hardeylim@gmail.com>
2023-10-09 18:19:01 +0800

    Enable bullet in dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,12 +2,14 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
+    Bullet.enable = true
+    Bullet.sentry = true
+    Bullet.alert = true
     Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+    Bullet.skip_html_injection = false
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
